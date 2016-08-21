4c2280fb731b8f4a221b394100f01707373903fc
Tuan Nguyen <nguyentuan92tg@gmail.com>
2016-08-21 15:02:52 +0700

    Add status validations and test

--- app/models/status.rb
+++ app/models/status.rb
@@ -1,4 +1,7 @@
 class Status < ApplicationRecord
 	#attr_accessible :content
 	belongs_to :user
+	validates :content, presence: true,
+						length: { minimum: 2}
+	validates :user_id, presence: true
 end
--- test/controllers/statuses_controller_test.rb
+++ test/controllers/statuses_controller_test.rb
@@ -12,12 +12,13 @@ class StatusesControllerTest < ActionDispatch::IntegrationTest
 
   test "should get new" do
     get new_status_url
-    assert_response :success
+    assert_response :redirect
+    assert_redirected_to login_path
   end
 
   test "should create status" do
     assert_difference('Status.count') do
-      post statuses_url, params: { status: { content: @status.content, name: @status.name } }
+      post statuses_url, params: { status: { content: @status.content} }
     end
 
     assert_redirected_to status_url(Status.last)
@@ -34,7 +35,7 @@ class StatusesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update status" do
-    patch status_url(@status), params: { status: { content: @status.content, name: @status.name } }
+    patch status_url(@status), params: { status: { content: @status.content} }
     assert_redirected_to status_url(@status)
   end
 
